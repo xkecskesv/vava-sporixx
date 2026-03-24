d10effe2854e8c2ae2d7d2706260eac01895379b
Marek Rakús <151932989+marekrakus03@users.noreply.github.com>
2026-03-24 12:20:19 +0100

    pridané fonty, upraveny css a predbezne hotový login screen

--- src/main/java/sk/stuba/fiit/sporixx/Main.java
+++ src/main/java/sk/stuba/fiit/sporixx/Main.java
@@ -1,7 +1,33 @@
 package sk.stuba.fiit.sporixx;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
-public class Main {
+/*DOCASNY MAIN - TREBA HO NAHRADIT*/
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        loadFonts();
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login_basic.fxml"));
+        Scene scene = new Scene(root, 1250, 920);
+        stage.setTitle("Šporixx");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        // spustenie aplikácie
+        launch(args);
+    }
+
+    private void loadFonts(){
+        Font font_semi = Font.loadFont(getClass().getResourceAsStream("/assets/fonts/Inter_28pt-SemiBold.ttf"), 12);
+        Font font_reg = Font.loadFont(getClass().getResourceAsStream("/assets/fonts/Inter_28pt-Regular.ttf"), 12);
+        Font font_bold = Font.loadFont(getClass().getResourceAsStream("/assets/fonts/Inter_28pt-Bold.ttf"), 12);
     }
 }
