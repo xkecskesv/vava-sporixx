ddf4d6e5dfc1d834cc96a089584cde129dfff3c1
Marek Rakús <151932989+marekrakus03@users.noreply.github.com>
2026-03-29 15:13:58 +0200

    Základné hardcoded UI, grafu (analytics) a aktivít

--- src/main/java/sk/sporixx/ui/DashboardController.java
+++ src/main/java/sk/sporixx/ui/DashboardController.java
@@ -1,25 +1,43 @@
 package sk.sporixx.ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 public class DashboardController {
 
     @FXML private VBox cardMain;
     @FXML private VBox cardEmergency;
+    @FXML private LineChart<String, Number> analyticsChart;
 
     @FXML
     public void initialize() {
         cardMain.setOnMouseClicked(e -> selectCard(cardMain));
         cardEmergency.setOnMouseClicked(e -> selectCard(cardEmergency));
+        loadAnalyticsChart();
+    }
+
+    private void loadAnalyticsChart() {
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>("Jan", 3200));
+        series.getData().add(new XYChart.Data<>("Feb", 4100));
+        series.getData().add(new XYChart.Data<>("Mar", 3800));
+        series.getData().add(new XYChart.Data<>("Apr", 5200));
+        series.getData().add(new XYChart.Data<>("May", 6800));
+        series.getData().add(new XYChart.Data<>("Jun", 7200));
+        series.getData().add(new XYChart.Data<>("Jul", 9800));
+        series.getData().add(new XYChart.Data<>("Aug", 9500));
+        series.getData().add(new XYChart.Data<>("Sep", 10200));
+        series.getData().add(new XYChart.Data<>("Oct", 8800));
+        series.getData().add(new XYChart.Data<>("Nov", 7500));
+        series.getData().add(new XYChart.Data<>("Dec", 8900));
+        analyticsChart.getData().add(series);
     }
 
     private void selectCard(VBox selected) {
-        // Zoznam všetkých kariet
         VBox[] cards = { cardMain, cardEmergency };
 
         for (VBox card : cards) {
@@ -27,7 +45,6 @@ public class DashboardController {
                 card.getStyleClass().clear();
                 card.getStyleClass().add("account-card-active");
 
-                // Prepni labely na aktívne štýly
                 for (var node : card.getChildren()) {
                     if (node instanceof Label label) {
                         String text = label.getText();
