1c0230e7807a8d6a7098a5a39101bf0f1034642b
Adela Kudláčová <adelkakudlacova@gmail.com>
2026-05-04 17:54:26 +0200

    odstránenie notifikácii

--- src/main/java/sk/sporixx/service/SettingsService.java
+++ src/main/java/sk/sporixx/service/SettingsService.java
@@ -12,26 +12,6 @@ public interface SettingsService {
 
     void setCurrencyCode(String currencyCode);
 
-    boolean isUpcomingPaymentsEnabled();
-
-    void setUpcomingPaymentsEnabled(boolean enabled);
-
-    boolean isBudgetLimitAlertsEnabled();
-
-    void setBudgetLimitAlertsEnabled(boolean enabled);
-
-    boolean isSavingRemindersEnabled();
-
-    void setSavingRemindersEnabled(boolean enabled);
-
-    boolean isSavingGoalsUpdatesEnabled();
-
-    void setSavingGoalsUpdatesEnabled(boolean enabled);
-
-    boolean isAchievementsEnabled();
-
-    void setAchievementsEnabled(boolean enabled);
-
     UserSettings getSettingsSnapshot();
 
     void reload();
